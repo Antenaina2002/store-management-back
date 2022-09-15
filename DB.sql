7d5c01ab5d7e61ebb919150e17acb521259e2ddb
Antenaina2002 <99983482+Antenaina2002@users.noreply.github.com>
2022-09-15 21:24:43 +0100

    Add files via upload

---
+++ DB.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE manage;
+
+CREATE TABLE product (
+    user_id serial PRIMARY KEY,
+	product_name VARCHAR ( 50 ) NOT NULL,
+	price INT NOT NULL, 
+);
+
+insert into product (product_id, product_name, price) values (1, 'Truffle - Whole Black Peeled', 24);
+insert into product (product_id, product_name, price) values (2, 'Pork - Back, Long Cut, Boneless', 32);
+insert into product (product_id, product_name, price) values (3, 'Sprouts Dikon', 44);
+insert into product (product_id, product_name, price) values (4, 'Bread Sour Rolls', 43);
+insert into product (product_id, product_name, price) values (5, 'Beans - Black Bean, Dry', 12);
+insert into product (product_id, product_name, price) values (6, 'Fireball Whisky', 35);
+insert into product (product_id, product_name, price) values (7, 'Juice - Ocean Spray Kiwi', 11);
+insert into product (product_id, product_name, price) values (8, 'Hold Up Tool Storage Rack', 29);
+insert into product (product_id, product_name, price) values (9, 'Tomatoes - Vine Ripe, Yellow', 37);
+insert into product (product_id, product_name, price) values (10, 'Oil - Pumpkinseed', 36);
+insert into product (product_id, product_name, price) values (11, 'Lemonade - Black Cherry, 591 Ml', 43);
+insert into product (product_id, product_name, price) values (12, 'Olives - Kalamata', 36);
+insert into product (product_id, product_name, price) values (13, 'Celery Root', 15);
+insert into product (product_id, product_name, price) values (14, 'Salt - Rock, Course', 18);
+insert into product (product_id, product_name, price) values (15, 'Calypso - Lemonade', 14);
+insert into product (product_id, product_name, price) values (16, 'Quiche Assorted', 33);
+insert into product (product_id, product_name, price) values (17, 'Icecream - Dstk Cml And Fdg', 37);
+insert into product (product_id, product_name, price) values (18, 'Lidsoupcont Rp12dn', 26);
+insert into product (product_id, product_name, price) values (19, 'Croissant, Raw - Mini', 31);
+insert into product (product_id, product_name, price) values (20, 'Monkfish - Fresh', 10);
+insert into product (product_id, product_name, price) values (21, 'Knife Plastic - White', 47);
+insert into product (product_id, product_name, price) values (22, 'Dawn Professionl Pot And Pan', 35);
+insert into product (product_id, product_name, price) values (23, 'Pasta - Spaghetti, Dry', 26);
+insert into product (product_id, product_name, price) values (24, 'Butter - Unsalted', 5);
+insert into product (product_id, product_name, price) values (25, 'Parasol Pick Stir Stick', 6);
+insert into product (product_id, product_name, price) values (26, 'Tortillas - Flour, 10', 3);
+insert into product (product_id, product_name, price) values (27, 'Lamb - Bones', 40);
+insert into product (product_id, product_name, price) values (28, 'Cheese - Bakers Cream Cheese', 4);
+insert into product (product_id, product_name, price) values (29, 'Trout - Smoked', 27);
+insert into product (product_id, product_name, price) values (30, 'Scallops - 20/30', 12);
+insert into product (product_id, product_name, price) values (31, 'Lamb - Shoulder', 45);
+insert into product (product_id, product_name, price) values (32, 'Juice - Apple, 500 Ml', 24);
+insert into product (product_id, product_name, price) values (33, 'Cleaner - Pine Sol', 32);
+insert into product (product_id, product_name, price) values (34, 'Sauce - Hoisin', 24);
+insert into product (product_id, product_name, price) values (35, 'Wine - Magnotta - Red, Baco', 11);
+insert into product (product_id, product_name, price) values (36, 'Ginger - Pickled', 27);
+insert into product (product_id, product_name, price) values (37, 'Bread Crumbs - Panko', 15);
+insert into product (product_id, product_name, price) values (38, 'Scotch - Queen Anne', 4);
+insert into product (product_id, product_name, price) values (39, 'Pasta - Ravioli', 7);
+insert into product (product_id, product_name, price) values (40, 'Bread Sour Rolls', 44);
+insert into product (product_id, product_name, price) values (41, 'Sauce - Salsa', 30);
+insert into product (product_id, product_name, price) values (42, 'Raisin - Dark', 36);
+insert into product (product_id, product_name, price) values (43, 'Wine - Red, Metus Rose', 8);
+insert into product (product_id, product_name, price) values (44, 'Sprite, Diet - 355ml', 37);
+insert into product (product_id, product_name, price) values (45, 'Wiberg Cure', 40);
+insert into product (product_id, product_name, price) values (46, 'Cheese - Parmesan Cubes', 13);
+insert into product (product_id, product_name, price) values (47, 'Currants', 27);
+insert into product (product_id, product_name, price) values (48, 'Glaze - Clear', 12);
+insert into product (product_id, product_name, price) values (49, 'Syrup - Monin, Irish Cream', 13);
+insert into product (product_id, product_name, price) values (50, 'Soup Bowl Clear 8oz92008', 37);
